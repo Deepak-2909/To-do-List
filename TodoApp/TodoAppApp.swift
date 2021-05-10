2f53ca4a26181f1232d72c14c87212a7d38885bc
Deepak2909 <deepak19csu075@ncuindia.edu>
2021-05-11 02:08:49 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Deepak on 11/05/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
